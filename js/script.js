42d4a50ca642167ad867e6857368fa0f78f64a26
Karla Agraz <karla.agraz@gmail.com>
2021-03-04 11:49:39 -0500

    animaciones para el loader

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+/* Loader */
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('body').classList.add
+        ('display')
+    }, 4000)
+}
+
+/* Hamburger Menu */
 document.querySelector('.hamburger-menu').
 addEventListener('click', () => {
     document.querySelector('.container').classList.toggle
     ("change")
 })
 
-
+/* Scroll button */
 document.querySelector('.scroll-btn').addEventListener
 ('click', () => {
     document.querySelector('html').style.scrollBehavior
